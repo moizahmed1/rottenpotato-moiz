90081835a65adaeb436550223f39105413ea96ad
Moiz <mahmed24@uncc.edu>
2016-10-22 02:39:38 +0000

    Added the delete a movie feature

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -34,6 +34,13 @@ flash[:notice] = "#{@movie.title} was successfully updated."
  redirect_to movie_path(@movie)
  end
 
+def destroy
+ @movie = Movie.find(params[:id])
+ @movie.destroy
+ flash[:notice] = "Movie ’#{@movie.title}’ deleted."
+ redirect_to movies_path
+end
+
 
 end
 
